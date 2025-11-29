4d4230b255427b3614f091539cd595c4d1b75bd4
Mat <mjadams421@gmail.com>
2025-11-29 00:35:33 +0000

    Added to the game

---
+++ style/java.js
@@ -0,0 +1,16 @@
+//house option
+house=document.getElementById('option1');
+
+function switchA {
+ house.style.fontSize='100px';
+ house.innerHTML='You entered the house';
+}
+
+ house.addEventListener('click',switchA);
+
+//underground celler option
+celler=document.getElementById('option2');
+
+
+//leave the area option
+leave=document.getElementById('option3');
